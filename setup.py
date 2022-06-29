5c0b7b87429cac1b27b8df70f6d807aeafae00ce
Wei Mingzhi <whistler@member.fsf.org>
2022-06-29 17:39:23 +0800

    version 7, corresponding to apollo 7.0

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name = 'cyber-record-util',
-    version = '0.1',
+    version = '7',
     description = 'standalone apollo cyber record reader/writer',
     author = 'Wei Mingzhi',
     author_email = 'whistler@member.fsf.org',
